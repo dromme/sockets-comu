aa5d46151110db173f218bc8f8195f5b19c1d567
Meli <melibarba4@gmail.com>
2018-08-28 21:16:18 -0500

    Cambié el astronauta y algunos iconos

--- public/js/game.js
+++ public/js/game.js
@@ -22,10 +22,10 @@ var background;
 
 function preload() {
   this.load.image('star', 'assets/sat.png');
-  this.load.image('comet', 'assets/falling.png');
+  this.load.image('comet', 'assets/ufo.png');
   this.load.image('sky', 'assets/sky.png');
-  this.load.image('astro', 'assets/astron.png');
-  this.load.spritesheet('dude','assets/dude0.png', { frameWidth: 32, frameHeight: 48 } );
+  this.load.image('astro', 'assets/ship.png');
+  this.load.spritesheet('dude','assets/dude4.png', { frameWidth: 37, frameHeight: 56 } );
 }
 
 function create() {
@@ -168,7 +168,7 @@ function addPlayer(self, playerInfo) {
  
   self.player.setGravity(200,-500);
   if (playerInfo.team === 'blue') {
-    self.player.setTint(0xa8f2f7);
+    self.player.setTint(0xa54cff);
   } else {
    self.player.setTint(0xff664c);
   }
@@ -180,7 +180,7 @@ function addPlayer(self, playerInfo) {
 function addOtherPlayers(self, playerInfo) {
   const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'dude');
   if (playerInfo.team === 'blue') {
-    otherPlayer.setTint(0x42bff4);
+    otherPlayer.setTint(0x5900d6);
   } else {
     otherPlayer.setTint(0xed391a);
   }
