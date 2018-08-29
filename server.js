abcd74a471143117fa1e8b4ff22138b0c6ecc800
Pedro Gallego <pedro.gallego@udea.edu.co>
2018-08-29 01:55:47 -0500

    sd

--- server.js
+++ server.js
@@ -59,10 +59,12 @@ io.on('connection', function (socket) {
 
         // when a player moves, update the player data
         socket.on('playerMovement', function (movementData) {
-            players[socket.id].x = movementData.x;
-            players[socket.id].y = movementData.y;
-            // emit a message to all players about the player that moved
-            socket.broadcast.emit('playerMoved', players[socket.id]);
+            if(players[socket.id] !== undefined) {
+                players[socket.id].x = movementData.x;
+                players[socket.id].y = movementData.y;
+                // emit a message to all players about the player that moved
+                socket.broadcast.emit('playerMoved', players[socket.id]);
+            }
         });
 
         socket.on('starCollected', function () {
